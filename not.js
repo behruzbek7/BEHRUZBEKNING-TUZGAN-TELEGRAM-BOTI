234ee8a5da30d83094e131133fb12f41d23d3c59
Behruzbek <sultonmurotovbehruzbek2010@gmail.com>
2025-12-01 17:38:00 +0500

    xabar jonatish

--- not.js
+++ not.js
@@ -1,17 +1,8 @@
-import TelegramBot from "node-telegram-bot-api";
-
-const TOKEN = "7568316605:AAFseC-EHV2tE2wF2RRZHuarT4IYJFBD3mk";
-
-const bot =TelegramBot(TOKEN,{polling: true});
-
-bot.on("Message", function(msg) {
-    const chatID =msg.chat.id;
-    bot.sendMessage(chatID,"Salom");
-});
+console.log("Bot ishga tushdi...");
+import "./src/bot/bot.js";
+import dotenv from "dotenv";
 
 
+dotenv.config();
 
-
-
-
- console.log("Bot ishga tushdi0................")
+console.log("Dastur ishga tushmoqda...");
